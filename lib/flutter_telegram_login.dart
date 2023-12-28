cc5899c446fabb82222c12f853f9aeefdc5afe00
NHOR SOPHEAK <mr.nhorsopheak@gmail.com>
2023-12-28 12:11:26 +0700

    firt commit

---
+++ lib/flutter_telegram_login.dart
@@ -0,0 +1,10 @@
+library flutter_telegram_login;
+
+import 'dart:math';
+
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+
+part 'src/helper.dart';
+part 'src/session.dart';
+part 'src/telegram_manager.dart';
